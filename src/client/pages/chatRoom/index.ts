7650e6464db3caae8b1837432f8a549e102c9148
Emma <emma801006@hotmail.com>
2024-05-05 16:50:00 +0800

    add socket connection between client/server

--- src/client/pages/chatRoom/index.ts
+++ src/client/pages/chatRoom/index.ts
@@ -1,4 +1,12 @@
 import "./index.css";
+import { io } from 'socket.io-client'
+
 import { name } from "@/utils";
 
 console.log("client side chatroom page", name);
+
+// create connection
+const clientIo = io()
+clientIo.on('join', (msg) => {
+  console.log(msg)
+})
